1ab9a8350f909007140be9ac6f8c5e9cef3e56f8
Igor Rodic <92062603+Iggy988@users.noreply.github.com>
2023-08-28 09:03:07 +0200

    Update global.dart

--- lib/global/global.dart
+++ lib/global/global.dart
@@ -28,4 +28,4 @@ double countStarsRating = 0.0;
 String titleStarsRating = "";
 
 String fcmServerToken =
-    'key=AAAAHD3IhJ8:APA91bHLwDojJC-Wpne5o9HleoFYUya1pNsy7_EITaoGCOVpHaOaNcCVfYfJMlR-O6hhVDSVE7XJEcoFUECZgUHVZO3nIbKoYaRPhdcOmzGhY9b95qZMdTBc28HNkBj4CKPZvHrH4hCQ';
+    '';
